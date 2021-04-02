e4bcd86ae48e6f479db17ae61edd99b4ee418e61
mantzaris <avmantzaris@gmail.com>
2021-04-02 09:32:59 -0400

    trying BootStrap for the image slide

--- myJS/slides.js
+++ myJS/slides.js
@@ -15,3 +15,21 @@ function showDivs(n) {
     }
     x[slideIndex-1].style.display = "block";
 }
+
+var slideIndexBS = 1;
+showDivsBS(slideIndexBS);
+
+function plusDivsBS(n) {
+    showDivsBS(slideIndexBS += n);
+}
+
+function showDivsBS(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlidesBS");
+    if (n > x.length) {slideIndexBS = 1}
+    if (n < 1) {slideIndexBS = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndexBS-1].style.display = "block";
+}
