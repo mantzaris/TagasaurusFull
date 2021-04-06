97f86077b579969aea09ea19166f94f196633e11
mantzaris <avmantzaris@gmail.com>
2021-04-06 12:15:22 -0400

    bootstrap carousel connected to the javascript function call that iterates the images

--- myJS/slides.js
+++ myJS/slides.js
@@ -1,22 +1,5 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.display = "block";
-}
-
 var slideIndexBS = 1;
+
 showDivsBS(slideIndexBS);
 
 function plusDivsBS(n) {
@@ -25,11 +8,12 @@ function plusDivsBS(n) {
 
 function showDivsBS(n) {
     var i;
-    var x = document.getElementsByClassName("mySlidesBS");
+    var x = document.getElementsByClassName("carousel-item");
     if (n > x.length) {slideIndexBS = 1}
     if (n < 1) {slideIndexBS = x.length} ;
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
-    }
+    }    
     x[slideIndexBS-1].style.display = "block";
+    
 }
