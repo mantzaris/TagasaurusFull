0e6fa961b4c8e4a2e0f8c3b53aab883419e83385
mantzaris <avmantzaris@gmail.com>
2021-04-08 01:37:48 -0400

    adding the text entry component with bootstrap although the form is not full bootstrap yet

--- myJS/slides.js
+++ myJS/slides.js
@@ -1,7 +1,6 @@
 var slideIndexBS = 1;
 
 const fs = require('fs');
-const { Z_BLOCK } = require('zlib');
 const dir = './images'
 const files = fs.readdirSync(dir)
 
@@ -11,14 +10,10 @@ function plusDivsBS(n) {
     showDivsBS(slideIndexBS += n);
 }
 
-function showDivsBS(n) {
-    var i;
-    var x = document.getElementsByClassName("carousel-item");
+function showDivsBS(n) {    
     if (n > files.length) {slideIndexBS = 1}
     if (n < 1) {slideIndexBS = files.length} ;
-     
-    x[0].style.display = "block";
-  
+          
     document.getElementById('img1').src = `./images/${files[slideIndexBS - 1]}`;
         
 }
