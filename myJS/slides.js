1fb47c1091f13c9e3d295bb53e9c1f03e869f8d1
mantzaris <avmantzaris@gmail.com>
2021-04-14 21:51:12 -0400

    have a single JSON to hold the image attributes entered for each photo, and the key is the file name for each image. the values for the emotions and the edges are now also key valued

--- myJS/slides.js
+++ myJS/slides.js
@@ -6,31 +6,38 @@ const files = fs.readdirSync(dir)
 
 var processed_tag_word_list
 
+var image_states_JSON = {}
+
+
 //init methods to run upon loading
 showDivsBS(slideIndexBS);
 meme_fill()
 
 //called by the SAVE button to produce a JSON of the picture description state
 function savePicState() {
-
     //slider bar ranges stored in an array
-    emotion_value_array = [ document.getElementById('happyRangeID').value, document.getElementById('sadRangeID').value, document.getElementById('confusedRangeID').value ]
-
+    emotion_value_array = { happy: document.getElementById('happy').value, sad: document.getElementById('sad').value, 
+                                confused: document.getElementById('confused').value }
     //meme selection switch check boxes
-    meme_switch_booleans = []
+    meme_switch_booleans = {}
     for (var ii = 0; ii < files.length; ii++) {
-        meme_switch_booleans.push( document.getElementById(`memeSwitch${ii}`).checked )     
-    }    
-
+        meme_boolean_tmp =  document.getElementById(`${files[ii]}`).checked
+        meme_switch_booleans[`${files[ii]}`] = meme_boolean_tmp
+    }
     //the picture file name in context
     image_name = `${files[slideIndexBS - 1]}`
 
-    image_state_JSON = { imageName: image_name, tags: processed_tag_word_list, emotionalValueVector: emotion_value_array, memeChoices: meme_switch_booleans }
+    image_state_JSON = { imageName: image_name, tags: processed_tag_word_list, 
+                            emotionalValueVector: emotion_value_array, memeChoices: meme_switch_booleans }
     
     imageJSON = { imageName: image_name, imageState: image_state_JSON }
-    console.log( JSON.stringify(imageJSON) )    
 
+    image_states_JSON[image_name] = image_state_JSON
+
+    console.log( JSON.stringify(image_states_JSON) )
 }
+
+
 
 function plusDivsBS(n) {
     showDivsBS(slideIndexBS += n);
@@ -39,12 +46,10 @@ function showDivsBS(n) {
     if (n > files.length) {slideIndexBS = 1}
     if (n < 1) {slideIndexBS = files.length} ;
           
-    document.getElementById('img1').src = `./images/${files[slideIndexBS - 1]}`;
-        
+    document.getElementById('img1').src = `./images/${files[slideIndexBS - 1]}`;        
 }
 
-function processTags() {
-        
+function processTags() {        
     user_description = document.getElementById('descriptionInput').value            
     new_user_description = remove_stopwords(user_description)
     
@@ -54,7 +59,6 @@ function processTags() {
     processed_tag_word_list = new_user_description.split(' ')
     
     savePicState()
-
 }
 
 function makeUL(array) {
@@ -69,15 +73,13 @@ function makeUL(array) {
         list.appendChild(item);
     }
     // Finally, return the constructed list:
-    return list;
-}
+    return list;}
 
 
 var stopwords = [' ','i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now']
 //removes via regex the special characters, and removes the stop words
 function remove_stopwords(str) {
-    res = []
-    
+    res = []    
     words = str.split(/[\s,./: ]+/);
     for(i=0;i<words.length;i++) {
        word_clean = words[i].split(".").join("")
@@ -90,17 +92,14 @@ function remove_stopwords(str) {
 
 
 function meme_fill() {
-
     document.getElementById('memes').innerHTML = ""
     meme_box = document.getElementById('memes')    
 
     for(ii=0;ii<files.length;ii++) {        
 
-        meme_box.insertAdjacentHTML('beforeend', `<input class="form-check-input" type="checkbox" value="" id="memeSwitch${ii}">
-                <img height="50%" width="80%" src="./images/${files[ii]}" /><br>  ` ); 
-                   
-    }
-    
+        meme_box.insertAdjacentHTML('beforeend', `<input class="form-check-input" type="checkbox" value="" id="${files[ii]}">
+                <img height="50%" width="80%" src="./images/${files[ii]}" /><br>  ` );                    
+    }    
 }
 
 //document.addEventListener('DOMContentLoaded', function() { meme_fill();}, false);
