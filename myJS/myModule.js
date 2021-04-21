4ba193591c7d303f8de2add8df2c7f7e9acf2900
mantzaris <avmantzaris@gmail.com>
2021-04-21 18:33:58 -0400

    persistance for the emotional values but it still needs to be loaded with the first image

--- myJS/myModule.js
+++ myJS/myModule.js
@@ -1,3 +1,5 @@
+const { exportDB } = require("dexie-export-import");
+
 exports.dbOpen = function(db__name) {
   //database name, Version number, Text description, Size of database
   let database = openDatabase(db_name, '1.0', 'stores image descriptions', 2 * 1024 * 1024);
@@ -16,6 +18,28 @@ exports.initDb = function(create_table_schema,table_name) {
 exports.objectType = function(target) {
   return Object.prototype.toString.call(target).replace(/\[object (.*)]/, '$1').toLowerCase()
 }
+
+exports.queryInsert = function(table_name,insert_into_statement,update_statement,image_name,emotion_value_array,meme_switch_booleans,processed_tag_word_list){
+        
+    query(
+        insert_into_statement,
+        [image_name,emotion_value_array,meme_switch_booleans,JSON.stringify(processed_tag_word_list)],
+        (a) => {
+            console.log('INSERT INTO: success')
+        },
+        (err) => {
+            if (err.message.indexOf('UNIQUE constraint failed') !== -1) {
+                query(
+                    update_statement,
+                    [ JSON.stringify(emotion_value_array), JSON.stringify(meme_switch_booleans),
+                        JSON.stringify( Object.assign({}, processed_tag_word_list) ),image_name]
+                    )
+            }else{
+                console.log("insert failed")
+            }
+    })
+}
+
 
 query = function(sql, params, success, fail) {
   if (fns_DB.objectType(params) === 'function') {
--- myJS/slides.js
+++ myJS/slides.js
@@ -9,11 +9,12 @@ table_name = 'table8'
 table_schema = '(name unique,emotions,memeChoices,tags)'
 table_col_names = '(name,emotions,memeChoices,tags)'
 create_table_schema = `CREATE TABLE IF NOT EXISTS ${table_name}${table_schema}`
+insert_into_statement = `INSERT INTO ${table_name} (name,emotions,memeChoices,tags) VALUES (?,?,?,?);`
+update_statement = `UPDATE ${table_name} SET emotions=?,memeChoices=?,tags=? WHERE name =?`
 db_name = 'mydb'
 const database = fns_DB.dbOpen(db_name)
 
 var processed_tag_word_list
-var image_states_JSON = {}
 var slideIndexBS = 1;
 
 //init methods to run upon loading
@@ -27,8 +28,7 @@ function savePicState() {
     emotion_value_array = {
         happy: document.getElementById('happy').value, sad: document.getElementById('sad').value,
         confused: document.getElementById('confused').value
-    }
-    console.log(processed_tag_word_list)
+    }    
     //meme selection switch check boxes
     meme_switch_booleans = {}
     for (var ii = 0; ii < files.length; ii++) {
@@ -40,48 +40,49 @@ function savePicState() {
     //the picture file name in context
     image_name = `${files[slideIndexBS - 1]}`
 
-    fns_DB.query(
-        `INSERT INTO ${table_name} (name,emotions,memeChoices,tags) VALUES (?,?,?,?);`,
-        [image_name,emotion_value_array,meme_switch_booleans,JSON.stringify(processed_tag_word_list)],
-        (a) => {
-            console.log('INSERT INTO: success')
-        },
-        (err) => {
-            if (err.message.indexOf('UNIQUE constraint failed') !== -1) {
-                fns_DB.query(
-                    `UPDATE ${table_name} SET emotions=?,memeChoices=?,tags=? WHERE name =?`,
-                    [ JSON.stringify(emotion_value_array), JSON.stringify(meme_switch_booleans),
-                        JSON.stringify( Object.assign({}, processed_tag_word_list) ),image_name]
-                    )
-            }else{
-                console.log("insert failed")
-            }
-    })    
+    fns_DB.queryInsert(table_name, insert_into_statement, update_statement, 
+                image_name, emotion_value_array, meme_switch_booleans, processed_tag_word_list)
 
-    select_res = undefined
+    
+
+}
+
+//called from the gallery widget
+function plusDivsBS(n) {
+    slideIndexBS += n;
+    if (slideIndexBS > files.length) {
+        slideIndexBS = 1
+    }
+    if (slideIndexBS < 1) {
+        slideIndexBS = files.length
+    };
+    document.getElementById('img1').src = `./images/${files[slideIndexBS - 1]}`;
+    
+
+    //set the emotional sliders values to the emotional vector values stored
     database.transaction(function (tx) {
-        tx.executeSql('SELECT * FROM table8', [ ], function(tx, results) {
-            console.log(results); select_res = results;
+        console.log(`SELECT * FROM ${table_name} WHERE name="${files[slideIndexBS-1]}"`)
+        tx.executeSql(`SELECT * FROM ${table_name} WHERE name="${files[slideIndexBS-1]}"`, [ ], function(tx, results) {
+            select_res = results;  
+            console.log(select_res.rows[0])
+            console.log( JSON.parse(select_res.rows[0]["emotions"]).happy ) 
+            console.log( JSON.parse(select_res.rows[0]["emotions"]).sad ) 
+            console.log( JSON.parse(select_res.rows[0]["emotions"]).confused ) 
+            document.getElementById('happy').value = JSON.parse(select_res.rows[0]["emotions"]).happy
+            document.getElementById('sad').value = JSON.parse(select_res.rows[0]["emotions"]).sad
+            document.getElementById('confused').value = JSON.parse(select_res.rows[0]["emotions"]).confused
         })
     });
 
 
-}
 
 
-function plusDivsBS(n) {
-    showDivsBS(slideIndexBS += n);
+    
+
+
 }
 
 function showDivsBS(n) {
-    if (n > files.length) {
-        slideIndexBS = 1
-    }
-    if (n < 1) {
-        slideIndexBS = files.length
-    }
-    ;
-
     document.getElementById('img1').src = `./images/${files[slideIndexBS - 1]}`;
 }
 
