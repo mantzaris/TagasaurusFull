10fc783bbd2cc374667869345b0545ac00bc5809
mantzaris <avmantzaris@gmail.com>
2021-05-18 00:16:53 -0400

    further reorganization of the functionality into different files

--- myJS/controller-main.js
+++ myJS/controller-main.js
@@ -14,6 +14,12 @@ const ipcRenderer = require('electron').ipcRenderer
 //module for the main annotation view alterations-directly affects the DOM
 const view_annotate_module = require('./myJS/view-annotate-module.js');
 
+//module for the functionality to export all the annotation data
+const data_export = require('./myJS/data-export.js')
+
+//module for helping the process of deleting an image and the references to it
+const delete_helper = require('./myJS/delete-helper.js')
+
 //module for the processing of the description
 const description_process_module = require('./myJS/descriptionProcessing.js');
 
@@ -86,7 +92,7 @@ function plusDivsBS(n) {
 async function firstDisplayInit(n) {        
     emotion_val_obj = {happy:0, sad:0, confused:0,descriptionInput:'', taglist:'', imgMain:files[n - 1]}
     view_annotate_module.Annotation_DOM_Alter(emotion_val_obj)
-    view_annotate_module.meme_fill(files)
+    view_annotate_module.Meme_View_Fill(files)
 
     var current_file_list = []
     //current_file_list =  getStoredFileNames(current_file_list)
@@ -113,7 +119,7 @@ async function loadNewImage() {
                 await getStoredFileNames(current_file_list).then()
                 checkAndHandleNewImages(current_file_list)                
                 refreshFileList()
-                view_annotate_module.meme_fill(files)
+                view_annotate_module.Meme_View_Fill(files)
             }
         });
     }
@@ -121,6 +127,10 @@ async function loadNewImage() {
 
 function refreshFileList() {
     files = fs.readdirSync(dir)
+}
+
+function ResetImage(){
+    view_annotate_module.Reset_Image_View(files)
 }
 
 
@@ -150,14 +160,12 @@ function checkAndHandleNewImages(current_file_list) {
         bool_new_file_name = current_file_list.some( name_tmp => name_tmp === `${files[ii]}` )
         
         if( bool_new_file_name == false ) {
-            
             //the picture file name in context
             image_name_tmp = `${files[ii]}`
             fns_DB.queryInsert(table_name, insert_into_statement, update_statement,
-                        image_name_tmp, JSON.stringify(emotion_value_array_tmp), 
-                        JSON.stringify(meme_switch_booleans_tmp), 
-                        processed_tag_word_list_tmp,rawDescription_tmp)      
-            
+                        image_name_tmp, JSON.stringify(emotion_value_array_tmp),
+                        JSON.stringify(meme_switch_booleans_tmp),
+                        processed_tag_word_list_tmp,rawDescription_tmp)
         }
     }    
 }
@@ -167,36 +175,7 @@ function loadStateOfImage() {
     
     database.transaction(function (tx) {
         tx.executeSql(`SELECT * FROM ${table_name} WHERE name="${files[slideIndexBS-1]}"`, [ ], function(tx, results) {
-            select_res = results;
-            if(select_res.rows.length > 0){
-                                
-                happy_val = JSON.parse(select_res.rows[0]["emotions"]).happy
-                sad_val = JSON.parse(select_res.rows[0]["emotions"]).sad
-                confused_val = JSON.parse(select_res.rows[0]["emotions"]).confused
-                tags_list = JSON.parse(select_res.rows[0]["tags"])
-                rawDescription_tmp = select_res.rows[0]["rawDescription"]
-                val_obj = {happy: happy_val, sad: sad_val, confused: confused_val,
-                                descriptionInput: rawDescription_tmp, taglist: tags_list}
-                
-                meme_json_parsed = JSON.parse(results.rows[0]["memeChoices"])
-                for (var ii = 0; ii < files.length; ii++) {
-                    if(document.getElementById(`${files[ii]}`) != null) { 
-                        if( (`${files[ii]}` in meme_json_parsed) ){                        
-                            if( meme_json_parsed[`${files[ii]}`] == true ){                                                            
-                                val_obj[`${files[ii]}`] = true
-                            } else{
-                                val_obj[`${files[ii]}`] = false
-                            }
-                        } else{
-                            val_obj[`${files[ii]}`] = false
-                        }
-                    }
-                }
-                view_annotate_module.Annotation_DOM_Alter(val_obj)
-            } else {                
-                emotion_val_obj = {happy: 0, sad: 0, confused: 0}
-                view_annotate_module.Annotation_DOM_Alter(emotion_val_obj)                
-            }
+            view_annotate_module.Display_Image_State_Results(files,results)
         })
     });
 
@@ -219,151 +198,19 @@ function processTags() {
 //delete image from user choice
 function Delete_Image() {    
     //try to delete the file (image)
-    try {
-        fs.unlinkSync( `./images/${files[slideIndexBS - 1]}` );
-        console.log(`File is deleted: ${files[slideIndexBS - 1]}`);
-    } catch (error) {
-        console.log(error);
-        console.log(`File was not deleted: ${files[slideIndexBS - 1]}`);
+    success = delete_helper.Delete_Image_File(files[slideIndexBS-1])
+    if(success == 1){
+        refreshFileList()
+        view_annotate_module.Meme_View_Fill(files)
+        //refresh the image view to the next image (which is by defaul the 'next' +1)
+        plusDivsBS( 0 ) 
+        //perform the house cleaning for the image references in the DB and the rest of the annotations
+        delete_helper.Image_Delete_From_DB_And_MemeRefs(table_name)    
     }
-
-    refreshFileList()
-    view_annotate_module.meme_fill(files)
-    //shift the image view to the next image
-    plusDivsBS( 1 ) 
-
-    //delete unecessary entries that don't connect to current files
-    Delete_DB_Unreferenced_Entries() 
-
-    //delete the meme references which do not reference files currently accessible
-    Delete_Void_MemeChoices()
-
-}
-
-//delete DB entries which are have no current image ref
-function Delete_DB_Unreferenced_Entries() {
-    //console.log( "in fn Delete_DB_Unreferenced_Entries" );
-    refreshFileList()
-    //console.log( files )
-    var all_db_filenames = ''
-    all_db_filenames_promise = new Promise( function(resolve, reject) {
-        database.transaction(function (tx) {
-            tx.executeSql(`SELECT name FROM "${table_name}"`, [ ], function(tx, results) {
-                //console.log("in SELECT results")
-                row_entries = Object.values(results.rows)                
-                db_filenames = row_entries.map(function(x){
-                    return x.name;
-                })                
-                resolve(db_filenames)
-            })
-        });
-    })
-
-    all_db_filenames_promise.then( function(result){        
-        all_db_filenames = result        
-        for(ii=0; ii<all_db_filenames.length; ii++){
-            //console.log(`file to check ; ${all_db_filenames[ii]}`)
-            in_or_not_bool = files.some(file_tmp => file_tmp == all_db_filenames[ii])
-            if(in_or_not_bool == false){
-                Delete_File_From_DB(all_db_filenames[ii])                
-            }
-        }
-    })
-    vanilla_notify.vNotify.success({visibleDuration: 1200,fadeOutDuration: 250,fadeInDuration: 250, text: 'Files deleted from database', title:'Deleted'});
-    return all_db_filenames
-}
-
-
-function Delete_File_From_DB(file_name){
-    database.transaction( function (tx) {
-        tx.executeSql(`DELETE FROM "${table_name}" WHERE name="${file_name}"`, [ ], function(tx, results) {
-            //
-        })
-    })
-}
-
-//get the name and memeChoices for each file to then update the entry with an altered memeChoice set 
-function Delete_Void_MemeChoices(){
-    //console.log('getting ready to get rid of the meme references which no longer are valid')
-    database.transaction( function (tx) {
-        tx.executeSql(`SELECT name,memeChoices FROM "${table_name}"`, [ ], function(tx, results) {
-            //console.log(results)
-            Void_MemeChoices_Helper(results.rows)
-        })
-    })
-}
-
-//get the names and memes and examine if they need updating
-function Void_MemeChoices_Helper(name_memes){
-
-    update_statement_memeChoices = `UPDATE ${table_name} SET memeChoices=? WHERE name =?`
-
-    for(ii=0; ii<name_memes.length; ii++){
-        parsed_memeChoices = JSON.parse(name_memes[ii].memeChoices)        
-        changed_memes = false
-        for (name_key in parsed_memeChoices) {
-            in_or_not_bool = files.some(file_tmp => file_tmp == name_key)
-            if(in_or_not_bool == false){
-                delete parsed_memeChoices[name_key] 
-                changed_memes = true
-            }        
-        }
-        if(changed_memes == true){
-            fns_DB.memeUpdate(update_statement_memeChoices, parsed_memeChoices, name_memes[ii].name)
-        }    
-    }
-
-}
-
-//function to reset annotations to default
-function ResetImage(){
-    val_obj = {happy: 0, sad: 0, confused: 0, descriptionInput:'', taglist:''}
-    for (var ii = 0; ii < files.length; ii++) {        
-        val_obj[`${files[ii]}`] = false
-    }    
-    view_annotate_module.Annotation_DOM_Alter(val_obj)
 }
 
 //functionality for the export of all the information
 function Export_All(){
-    const save_promise = ipcRenderer.invoke('dialog:save')
-    save_promise.then(function(path_chosen){ 
-        //get ready to export data
-        if(path_chosen.canceled == false){
-            if (!fs.existsSync(path_chosen.filePath)){
-                fs.mkdirSync(path_chosen.filePath);                
-                //call the DB to get the data dump and pass it to the 
-                database.transaction( function (tx) {
-                    tx.executeSql(`SELECT * FROM "${table_name}"`, [ ], function(tx, results) {
-                        //console.log( JSON.stringify(results.rows) )
-                        Write_Export_Data(path_chosen.filePath,results.rows)
-                    })
-                })                
-            } else {
-                vanilla_notify.vNotify.error({visibleDuration: 1200,fadeOutDuration: 250,fadeInDuration: 250, text: 'File or Folder already exists', title:'Canceled'});
-            }
-        } else{
-            vanilla_notify.vNotify.notify({visibleDuration: 1200,fadeOutDuration: 250,fadeInDuration: 250, text: 'No destination and folder name given', title:'Canceled'});
-        }
-    })
+    data_export.Export_User_Annotation_Data(table_name)
 }
-
-//put the annotation data to disk for the user's chosen folder
-function Write_Export_Data(file_path,db_rows){
-
-    //write the json data out to the folder
-    file_name_data = '/TagasaurusAnnotations.json'
-    fs.writeFileSync( file_path+file_name_data, JSON.stringify(db_rows) );
-    
-    //now copy the files as well to a new 'images' directory
-    fs.mkdirSync( file_path+'/images');
-    fse.copy( './images', file_path+'/images', err => {
-        if (err){ return console.error(err) }
-        else { console.log('folder copy success!') }
-    })
-    console.log("finished writing the annotations json file and copying images folder")
-
-}
-
-
 
--- myJS/view-annotate-module.js
+++ myJS/view-annotate-module.js
@@ -9,7 +9,7 @@ function Annotation_DOM_Alter(annotation_obj){
                 document.getElementById(key_tmp).innerHTML = annotation_obj[key_tmp]
             } else{
                 document.getElementById('taglist').innerHTML = ''
-                document.getElementById('taglist').appendChild(makeUL( annotation_obj[key_tmp] ))
+                document.getElementById('taglist').appendChild(Make_Tag_HTML_UL( annotation_obj[key_tmp] ))
             }
         } else if(key_tmp == 'descriptionInput'){
             document.getElementById(key_tmp).value = annotation_obj[key_tmp]
@@ -21,12 +21,44 @@ function Annotation_DOM_Alter(annotation_obj){
             document.getElementById(key_tmp).value = annotation_obj[key_tmp]
         }
     }
-
-
 }
 
+//set view to display image state results of query for a particular file name
+function Display_Image_State_Results(files,select_result){
+    if(select_result.rows.length > 0){
+                                
+        happy_val = JSON.parse(select_result.rows[0]["emotions"]).happy
+        sad_val = JSON.parse(select_result.rows[0]["emotions"]).sad
+        confused_val = JSON.parse(select_result.rows[0]["emotions"]).confused
+        tags_list = JSON.parse(select_result.rows[0]["tags"])
+        rawDescription_tmp = select_result.rows[0]["rawDescription"]
+        val_obj = {happy: happy_val, sad: sad_val, confused: confused_val,
+                        descriptionInput: rawDescription_tmp, taglist: tags_list}
+        
+        meme_json_parsed = JSON.parse(select_result.rows[0]["memeChoices"])
+        for (var ii = 0; ii < files.length; ii++) {
+            if(document.getElementById(`${files[ii]}`) != null) { 
+                if( (`${files[ii]}` in meme_json_parsed) ){                        
+                    if( meme_json_parsed[`${files[ii]}`] == true ){                                                            
+                        val_obj[`${files[ii]}`] = true
+                    } else{
+                        val_obj[`${files[ii]}`] = false
+                    }
+                } else{
+                    val_obj[`${files[ii]}`] = false
+                }
+            }
+        }
+        Annotation_DOM_Alter(val_obj)
+    } else {                
+        emotion_val_obj = {happy: 0, sad: 0, confused: 0}
+        Annotation_DOM_Alter(emotion_val_obj)                
+    }
+}
+
+
 //populate the meme switch view with images
-function meme_fill(files) {
+function Meme_View_Fill(files) {
     document.getElementById('memes').innerHTML = ""
     meme_box = document.getElementById('memes')
 
@@ -38,7 +70,7 @@ function meme_fill(files) {
 }
 
 //helper function to create the inner HTML for the tag list within the tag box
-function makeUL(tag_array) {
+function Make_Tag_HTML_UL(tag_array) {
     // Create the list element:
     var list = document.createElement('ul');
     for (var i = 0; i < tag_array.length; i++) {
@@ -53,7 +85,18 @@ function makeUL(tag_array) {
     return list;
 }
 
+//function to reset annotations to default
+function Reset_Image_View(files){
+    val_obj = {happy: 0, sad: 0, confused: 0, descriptionInput:'', taglist:''}
+    for (var ii = 0; ii < files.length; ii++) {        
+        val_obj[`${files[ii]}`] = false
+    }    
+    Annotation_DOM_Alter(val_obj)
+}
+
 
 
 exports.Annotation_DOM_Alter = Annotation_DOM_Alter
-exports.meme_fill = meme_fill
+exports.Meme_View_Fill = Meme_View_Fill
+exports.Display_Image_State_Results = Display_Image_State_Results
+exports.Reset_Image_View = Reset_Image_View
