720787cc22e4c0ad8621c9b4d51fc55cd52d2da7
mantzaris <avmantzaris@gmail.com>
2021-07-02 21:47:41 -0400

    the layout for the cells of the entity creation page is made. The basic elements within the cells are there to get an idea of the page components. The js file to register events from button clicks are there as well now.

--- AppCode/myJS/entity-view.js
+++ AppCode/myJS/entity-view.js
@@ -2,3 +2,30 @@
 
 console.log('in entity view')
 
+function Prev_Image() {
+    console.log("previous image button clicked")
+}
+
+function Next_Image() {
+    console.log("next image button clicked")
+}
+
+function Create_New_Entity() {
+    console.log("create new entity button clicked")
+}
+
+function Alter_Entity() {
+    console.log("alter entity button clicked")
+}
+
+function Entity_Emotion_Page() {
+    console.log("entity emotion page button clicked")
+}
+
+function Entity_Description_Page() {
+    console.log("entity description page button clicked")
+}
+
+function Entity_Memes_Page() {
+    console.log("entity memes page button clicked")
+}
