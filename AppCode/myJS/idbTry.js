f73238b1893a3e79933ca949f7c027bd53493b81
mantzaris <avmantzaris@gmail.com>
2021-07-10 19:42:18 -0400

    now the boiler plate code has all the essential methods ready

--- AppCode/myJS/idbTry.js
+++ AppCode/myJS/idbTry.js
@@ -28,221 +28,244 @@ db_entities = null;
 
 const ENTITY_DB_NAME = 'testEntityKP'
 const ENTITY_OBJSTORE_NAME = 'entitiesTmp'
+const ENTITY_KEY_PATH_NAME = "entityName" //primary key for records
 
+//create the database and the objectstore (table), and the keyPath (primary key) as the entity name
+function Create_Db(){
 
-function create_db(){
-    const request = window.indexedDB.open(ENTITY_DB_NAME,1)
+    let request = window.indexedDB.open(ENTITY_DB_NAME,1)
     
-    request.onerror = function(event){
-        console.log("problem opening db_entities.");
-    }
-
-    //opening
-    request.onupgradeneeded = function(event){
-        console.log('!!!UPGRADE!!!')
-        console.log( db_entities )
-        console.log(event.target.result.name)
-        //if db_entities exists and tablename/object store the same do not recreate
-        if(event.oldVersion > 0) {
-            console.log("IN IF!!!")
-            console.log('in upgradeneeded db_entities table name check!!!')
-            console.log(event.target.result.name)
-            
-        } else{
-            console.log("IN ELSE!!!")
-            db_entities = event.target.result;
-            const store = db_entities.createObjectStore(ENTITY_OBJSTORE_NAME,{keyPath: "entityName"})
-            store.transaction.oncomplete = function(event){
-                console.log("entitiesTmp store successfully completed UPGRADENEEDED")
-                //thisDB = event.target.result
-                //if(!thisDB.objectStoreNames.contains("roster")){
-                //    var peoplenames = thisDB.createObjectStoreNames("roster",{keyPath:"name"})
-                //    peoplenames.createIndex("name","name",{unique:false})
-                //    peoplenames.createIndex("dob","dob",{unique:false,multiEntry:true})
-                //}
-
-            }     
-        }
-    }
-
     request.onsuccess = function(event){
         db_entities = event.target.result;
-        console.log("successfully opened DB (onsuccess called already there)")
-
-        
-
+        console.log(`entity, Create_Db() successfully opened DB, ${ENTITY_DB_NAME}`)
+        console.log(event.target.result)
     }
-
+    request.onerror = function(event){
+        console.log(`entity, Create_Db() error opening a db, ${ENTITY_DB_NAME}`);
+        console.log(event.target.error)
+    }
+    
+    request.onupgradeneeded = function(event){
+        console.log('entity, Create_Db() onupgradeneeded')
+        console.log(event.target.result.name)
+        console.log('printing out db_entities object (db object) ', db_entities)
+        
+        //if db_entities exists and tablename/object store the same do not recreate
+        version_num = event.oldVersion
+        if(version_num > 0) { //db exists
+            db_entities = event.target.result;
+            console.log(`entities, onupgradeneeded in Create_Db() the event.oldVersion value= ${version_num}`)  
+            console.log('in upgradeneeded db_entities table name check!!!')
+            console.log(event.target.result)
+            console.log(event.target.result.name)
+        } else{ //new db, so time to create it from scratch, oldVersion is ZERO
+            console.log(`entities, create NEW db in Create_Db() the 'event.oldVersion' value= ${version_num}`)
+            db_entities = event.target.result;
+            store = db_entities.createObjectStore(ENTITY_OBJSTORE_NAME,{keyPath: ENTITY_KEY_PATH_NAME})
+            store.transaction.oncomplete = function(event){
+                console.log("entitiesTmp store creation successfully completed in onupgradeneeded")
+                console.log(event.target.result)
+            }
+        }
+    }
 }
-
 
 //incase we need to get rid of the database
 function Delete_Db(){
     let request = window.indexedDB.deleteDatabase(ENTITY_DB_NAME)
     request.onsuccess = function(event){
-        console.log(`the Database for the entities, ${ENTITY_DB_NAME} successfully deleted`)
-        console.log(event.target.error)
+        console.log(`entity, the Database for the entities, ${ENTITY_DB_NAME} successfully deleted`)
+        console.log(request.result)
     }
     request.onerror = function(event){
-        console.log(`the Database for the entities, ${ENTITY_DB_NAME} NOT successfully deleted`)
+        console.log(`entity, the Database for the entities, ${ENTITY_DB_NAME} NOT successfully deleted`)
         console.log(event.target.error)
     }
 }
 
-
 //eg. insert_record([{name:'doug',dob:'12/34/56',email:'abc@def.com'}])
 //fails for duplicate keys
-function Insert_Record(records){
+function Insert_Record(records) {
     if(db_entities){
         const insert_transaction = db_entities.transaction(ENTITY_OBJSTORE_NAME, 'readwrite')
         const store = insert_transaction.objectStore(ENTITY_OBJSTORE_NAME)
 
         insert_transaction.onerror = function(event){
-            console.log("error in -transaction- for insert_record, event.target.error to follow: ")
+            console.log("entity, error in -transaction- for insert_record, event.target.error to follow: ")
             console.log(event.target.error)
         }
         insert_transaction.oncomplete = function(){
-            console.log('all entity insert transactions complete')
+            console.log('entity, all entity insert transactions complete')
         }
 
         if(Array.isArray(records) == true){ //records is an array of multiple objects
             records.forEach(record => {
                 request = store.add(record);
                 request.onerror = function(event){
-                    console.log('could not add/insert into object store, record: ', record)
+                    console.log('entity, could not add/insert into object store, record: ', record)
+                    console.log(event.target.error)
                 }
                 request.onsuccess = function(){
-                    console.log('successfully added/inserted record: ', record)
+                    console.log('entity, successfully added/inserted record: ', record)
+                    console.log(request.result)
                 }
             })
-        }else{ //records is not an array but a single json object to insert
+        } else{ //records is not an array but a single json object to insert
             request = store.add(records);
             request.onerror = function(event){                
-                console.log('error in -store operation- add/insert in function insert_record (event.target.error to follow), this record: ', records)
+                console.log('entity, error in -store operation- add/insert in function insert_record (event.target.error to follow), this record: ', records)
                 console.log(event.target.error)
             }
             request.onsuccess = function(){
-                console.log('successfully added/inserted record: ', records)
+                console.log('entity, successfully added/inserted record: ', records)
+                console.log(request.result)
             }
         }
     }
 }
 
 //eg. get_record('abc@def.com')
-function get_record(email){
-
+function Get_Record(record_key){
 
     if(db_entities){
         const get_transaction = db_entities.transaction(ENTITY_OBJSTORE_NAME, 'readonly')
         const store = get_transaction.objectStore(ENTITY_OBJSTORE_NAME)
 
-        get_transaction.onerror = function(){
-            console.log('problem with transactions.')
+        get_transaction.onerror = function(event){
+            console.log(`entity, problem with transactions for Get_Record of ${record_key} , error report:`)
+            console.log(event.target.error)
         }
-
-        get_transaction.oncomplete = function(){
-            console.log('all transactions complete')
-        }
-
-    
-        let request = store.get(email);
-        
-        request.onerror = function(event){
-            console.log('could not get ', email)
-        }
-        request.onsuccess = function(event){
-            console.log('successfully got / retrieved ', email, ' ', event.target.result)
+        get_transaction.oncomplete = function(event){
+            console.log(`entity, all transactions complete for Get_Record of ${record_key}`)
             console.log(request.result)
         }
-    
         
+        let request = store.get(record_key);
+        
+        request.onerror = function(event){
+            console.log('entity, could not get record from store in Get_Record by key: ', record_key)
+            console.log(event.target.error)
+        }
+        request.onsuccess = function(event){
+            console.log('entity, successfully got / retrieved ', record_key, ' ', event.target.result)
+            // PUT HERE THE CALL BACK FOR THE RESULT TO BE ACTED UPON
+        }
     }
-
-
 }
 
 //eg. update_record({name:'scifi',dob:'22/11/80',email:'yea@lll.org'})
-//will also insert if the key variable value is different
-function update_record(record){
-
+function Update_Record(record){
 
     if(db_entities){
         const update_transaction = db_entities_entities.transaction(ENTITY_OBJSTORE_NAME, 'readwrite')
         const store = update_transaction.objectStore(ENTITY_OBJSTORE_NAME)
 
-        update_transaction.onerror = function(){
-            console.log('problem with transactions.')
+        update_transaction.onerror = function(event){
+            console.log('entity, problem with transactions in function Update_Record() for record: ', record)
+            console.log(event.target.error)
         }
-
-        update_transaction.oncomplete = function(){
-            console.log('all transactions complete')
+        update_transaction.oncomplete = function(event){
+            console.log('entity, all transactions complete in function Update_Record()', ' ', event.target.result)
         }
 
         let request = store.put(record);
 
         request.onerror = function(event){
-            console.log('could not update/put ', record)
+            console.log('entity, could not update/put in Update_Record(), record', record)
+            console.log(event.target.error)
         }
-        request.onsuccess = function(){
-            console.log('successfully updated/put ', record)
+        request.onsuccess = function(event){
+            console.log('entity, successfully updated/put in Update_Record(), record: ', record, ' ', event.target.result)
         }
-    
     }
 }
 
 //eg. delete_record('yeah@lll.org')
-//if the record is not there it returns successfully for the absence of the key
-function delete_record(email){
+function Delete_Record(record_key){
 
     if(db_entities){
-        const delete_transaction = db_entities.transaction(ENTITY_OBJSTORE_NAME, 'readwrite')
-        const store = delete_transaction.objectStore(ENTITY_OBJSTORE_NAME)
+        let delete_transaction = db_entities.transaction(ENTITY_OBJSTORE_NAME, 'readwrite')
+        let store = delete_transaction.objectStore(ENTITY_OBJSTORE_NAME)
 
-        delete_transaction.onerror = function(){
-            console.log('problem with transactions.')
+        delete_transaction.onerror = function(event){
+            console.log('entity, problem with transactions for Delete_Record(), record: ', record_key)
+            console.log(event.target.error)
+        }
+        delete_transaction.oncomplete = function(event){
+            console.log('entity, all transactions complete for Delete_Record()', ' ', event.target.result)
         }
 
-        delete_transaction.oncomplete = function(){
-            console.log('all transactions complete')
-        }
-
-        let request = store.delete(email);
+        let request = store.delete(record_key);
 
         request.onerror = function(event){
-            console.log('could not delete ', email)
+            console.log('entity, could not delete record in Delete_Record, record: ', record_key)
+            console.log(event.target.error)
         }
-        request.onsuccess = function(){
-            console.log('successfully delete ', email)
-        }
-    
+        request.onsuccess = function(event){
+            console.log('entity, successfully delete ', record_key, ' ', event.target.result)
+        }    
     }
+}
 
+//return all the records in the object store 
+function Get_All_From_DB(){
+
+    let cursor_transaction = db_entities.transaction(ENTITY_OBJSTORE_NAME, 'readonly')
+    let store = cursor_transaction.objectStore(ENTITY_OBJSTORE_NAME)
+
+    let request = store.getAll();
+    request.onerror = function(event){
+        console.log('entity, could not Get_All_From_DB()')
+        console.log(event.target.error)
+    }
+    request.onsuccess = function(event) {
+        console.log('entity, success in Get_All_From_DB()')
+        console.log(event.target.result)
+    }
+}
+
+//return all the keys (primary keys) in the object store
+function Get_All_Keys_From_DB(index){
+
+    let cursor_transaction = db_entities.transaction(ENTITY_OBJSTORE_NAME, 'readonly')
+    let store = cursor_transaction.objectStore(ENTITY_OBJSTORE_NAME)
+
+    let request = store.getAllKeys();
+    request.onerror = function(event){
+        console.log('entity, could not IDBObjectStore.Get_All_Keys_From_DB()')
+        console.log(event.target.error)
+    }
+    request.onsuccess = function(event) {
+        console.log('entity, success in IDBObjectStore.Get_All_Keys_From_DB()')
+        console.log(event.target.result)
+    }
 }
 
 ii=0
 var docs = []
+//there is also 'IDBIndex.openKeyCursor'
 function cursor(){
 
     if(db_entities){
-        const cursor_transaction = db_entities.transaction(ENTITY_OBJSTORE_NAME, 'readonly')
-        const store = cursor_transaction.objectStore(ENTITY_OBJSTORE_NAME)
+        let cursor_transaction = db_entities.transaction(ENTITY_OBJSTORE_NAME, 'readonly')
+        let store = cursor_transaction.objectStore(ENTITY_OBJSTORE_NAME)
 
-        cursor_transaction.onerror = function(){
-            console.log('problem with cursor transactions.')
+        cursor_transaction.onerror = function(event){
+            console.log('entity, problem with cursor transactions.')
+            console.log(event.target.error)
+        }
+        cursor_transaction.oncomplete = function(event){
+            console.log('entity, all cursor transactions complete')
+            console.log(event.target.result)
         }
 
-        cursor_transaction.oncomplete = function(){
-            console.log('all cursor transactions complete')
-        }
-
-        let request = store.openCursor();
-        
-
+        let request = store.openCursor();        
         request.onerror = function(event){
-            console.log('could not make cursor ', event.target.result)
+            console.log('entity, could not make cursor ')
+            console.log(event.target.error)
         }
         request.onsuccess = function(event) {
-            var cursor = event.target.result;
+            let cursor = event.target.result;
             if(cursor) {
                 console.log(ii)
                 console.log(cursor.key)
@@ -250,56 +273,27 @@ function cursor(){
                 ii += 1
                 docs.push(cursor.value)
                 console.log(event)
-                // cursor.value contains the current record being iterated through
-                // this is where you'd do something with the result
-                //for (var field in cursor.value){
-                //console.log( 'cursor event result produced ' + cursor.value['name'] )
-                //}
+                
                 cursor.continue();
             } else {
-                console.log("no more results")// no more results
+                console.log("entity, no more cursor results")// no more results
             }
-        }
-    
+        }    
     }
-
 }
-
-
-function Get_All_From_DB(){
-
-    cursor_transaction2 = db_entities.transaction(ENTITY_OBJSTORE_NAME, 'readonly')
-    store2 = cursor_transaction2.objectStore(ENTITY_OBJSTORE_NAME)
-
-    request2 = store2.getAll();
-    request2.onsuccess = function(event2) {
-        console.log(event2.target.result)
-    }
-
-}
-//var request = objectStore.getAll();
-//var getAllKeysRequest = IDBIndex.getAll();
-//var myIndex = objectStore.index('index');
-//var getAllRequest = myIndex.getAll();
-//getAllRequest.onsuccess = function() {
-//  console.log(getAllRequest.result);
-//}
-//objectStore.getAll().onsuccess = function(event) {
-//    logTimestamps(event.target.result);
-//};
-//    var allRecords = store.getAll();
-//allRecords.onsuccess = function() {
-//    console.log(allRecords.result);
-//};
-//* IDBObjectStore.getAll()
-//* IDBObjectStore.getAllKeys()
-//* IDBIndex.getAll()
-//* IDBIndex.getAllKeys()
-
 
 
 //MUST RUN THIS FUNCTION FIRST TO GET THE DB OBJECT
-create_db()
+Create_Db()
 //delete_db()
 
 
+
+
+//* to implement in the future
+//* IDBIndex.getAll()
+//* IDBIndex.getAllKeys()
+//* IDBIndex.getAllKeys(keyRangeValue)
+//* ObjStore.openCursor(keyRangeValue)
+//* IDBIndex.openCursor(keyRangeValue)
+//https://stackoverflow.com/a/56729678/410975
