cfdc804c9d7284e13a8e51c27d0d4dc6acf7bd7c
mantzaris <avmantzaris@gmail.com>
2021-09-01 21:19:02 -0400

    cleaning up and refactored the entity controller js file. now the other related entity js files are coming up to be improved upon

--- AppCode/myJS/controller-entity.js
+++ AppCode/myJS/controller-entity.js
@@ -17,15 +17,15 @@ var current_entity_obj; //it holds the object of the entity being in current con
 var all_entity_keys; //holds all the keys to the entities in the DB
 var current_key_index = 0; //which key index is currently in view for the current entity
 
-
+//this function deletes the entity object currently in focus from var 'current_key_index', and calls for the refresh
+//of the next entity to be in view
 async function Delete_Entity() {
-
     entity_key = current_entity_obj.entityName
-
     await ENTITY_DB_FNS.Delete_Record(entity_key)
-    await ENTITY_DB_FNS.Get_All_Keys_From_DB()
-    all_entity_keys = ENTITY_DB_FNS.Read_All_Keys_From_DB()   
+    await ENTITY_DB_FNS.Get_All_Keys_From_DB() //refresh the current key list
+    all_entity_keys = ENTITY_DB_FNS.Read_All_Keys_From_DB() //retrieve that key list and set to the local global variable
 
+    //notification
     Toastify({
         text: "This is a toast", duration: 1500, newWindow: true,
         close: true, gravity: "top", position: "left",
@@ -33,93 +33,81 @@ async function Delete_Entity() {
         //onClick: function(){} // Callback after click
     }).showToast();
 
-    Show_Entity_From_Key(all_entity_keys[current_key_index])
+    Show_Entity_From_Key_Or_Current_Entity(all_entity_keys[current_key_index]) //current index for keys will be 1 ahead from before delete
 }
 
-function Save_Entity_Emotions() {
-    happy_value = document.getElementById('happy').value
-    sad_value = document.getElementById('sad').value
-    confused_value = document.getElementById('confused').value
-    current_entity_obj.entityEmotions = {happy:happy_value,sad:sad_value,confused:confused_value}
-    ENTITY_DB_FNS.Update_Record(current_entity_obj)
-}
-
-function Save_Entity_Description() {
-    current_entity_obj.entityDescription = document.getElementById("descriptionInputEntity").value
-    ENTITY_DB_FNS.Update_Record(current_entity_obj)
-
-}
-
+//choose a new entity meme set from an already built entity (replace the previous meme set)
 async function New_Entity_Memes(){
     result = await IPC_RENDERER_PICS.invoke('dialog:openEntityImageSet')
     files_tmp = result.filePaths
-    files_tmp_base = files_tmp.map(function(filePATH) {
+    files_tmp_base = files_tmp.map(function(filePATH) { //get an array of the base file paths chosen
         return PATH.parse(filePATH).base
     })
-
+    //handle images that may not be in the app's local image directory yet and copy over if needed
     if(files_tmp_base.length == 0){
-        console.log('empty meme array chosen')
+        //console.log('empty meme array chosen')
     } else {
-        directory_of_image = PATH.dirname(result.filePaths[0])
-        if(directory_of_image != DIR_PICS){//DIR_PICS
-            files_tmp_base = MY_FILE_HELPER.Copy_Non_Taga_Files(result,DIR_PICS)
+        directory_of_image = PATH.dirname(result.filePaths[0]) //get the directory of the images selected
+        if(directory_of_image != DIR_PICS){ //user did not select the taga image store
+            //this custom copy handles filename collisions by adding random salt to the file name before adding it if needed
+            files_tmp_base = MY_FILE_HELPER.Copy_Non_Taga_Files(result,DIR_PICS) //returns the new file names in local dir space
         } else{
-            console.log('files are in the taga images directory')
+            //console.log('files are in the taga images directory')
         }
     }
 
     current_entity_obj.entityMemes = files_tmp_base
-    ENTITY_DB_FNS.Update_Record(current_entity_obj)
-    gallery_html = `<div class="row" id="meme_page_view">`
-    default_PATH = __dirname.substring(0, __dirname.lastIndexOf('/')) + '/images/' 
-    memes_array = current_entity_obj.entityMemes
-    if(memes_array != ""){
-        memes_array.forEach(element => {
-            gallery_html += `
-            <img class="imgG" src="${default_PATH + element}">
-            `
-        });    
-    }
-    gallery_html += `<br><button type="button" class="btn btn-primary btn-lg" onclick="New_Entity_Memes()">Choose new memes</button>`
-    document.getElementById("annotationPages").innerHTML  = gallery_html;
+    ENTITY_DB_FNS.Update_Record(current_entity_obj) //update the DB with the new meme file names
+    Entity_Memes_Page() //update the meme annotation subview with the new updated entity meme records
 }
 
+//when the entity memes annotation page is select these page elements are present for the meme view
 function Entity_Memes_Page() {
+    //make only the meme view pagination button active and the rest have active removed to not be highlighted
     document.getElementById('entity-meme-view').className += " active";
     document.getElementById('entity-emotion-view').classList.remove("active")
     document.getElementById('entity-description-view').classList.remove("active")
     document.getElementById("annotationPages").textContent = "show the memes now"
-
-    memes_array = current_entity_obj.entityMemes
+    
+    memes_array = current_entity_obj.entityMemes //get the memes of the current object
 
     gallery_html = `<div class="row" id="meme_page_view">`
-    default_PATH = __dirname.substring(0, __dirname.lastIndexOf('/')) + '/images/' 
     memes_array = current_entity_obj.entityMemes
     if(memes_array != ""){
         memes_array.forEach(element => {
             gallery_html += `
-            <img class="imgG" src="${default_PATH + element}">
+            <img class="imgG" src="${DIR_PICS + '/' + element}">
             `
-        });    
+        });
     }
     gallery_html += `<br><button type="button" class="btn btn-primary btn-lg" onclick="New_Entity_Memes()">Choose new memes</button>`
     document.getElementById("annotationPages").innerHTML  = gallery_html;
 }
 
+//entity annotation page where the user describes the entity
 function Entity_Description_Page() {
-    document.getElementById('entity-description-view').className += " active";
+    document.getElementById('entity-description-view').className += " active"; //activate correct pagination label
     document.getElementById('entity-emotion-view').classList.remove("active")
-    document.getElementById('entity-meme-view').classList.remove("active")    
+    document.getElementById('entity-meme-view').classList.remove("active")
+
     description_HTML_str = '<textarea class="form-control textarea2" id="descriptionInputEntity" ></textarea>'
     description_HTML_str += `<button type="button" class="btn btn-primary btn-lg" onclick="Save_Entity_Description()">Save</button>`
     document.getElementById('annotationPages').innerHTML = description_HTML_str
     document.getElementById("descriptionInputEntity").value = current_entity_obj.entityDescription
 }
 
+//takes the current description and updates the entity object in the DB with it
+function Save_Entity_Description() {
+    current_entity_obj.entityDescription = document.getElementById("descriptionInputEntity").value
+    ENTITY_DB_FNS.Update_Record(current_entity_obj)
+}
+
+//create the entity emotion HTML view for the entity annotation
 function Entity_Emotion_Page() {
-    document.getElementById('entity-emotion-view').className += " active";
+    document.getElementById('entity-emotion-view').className += " active"; //activate the correct pagination label
     document.getElementById('entity-description-view').classList.remove("active")
     document.getElementById('entity-meme-view').classList.remove("active")
+
     emotion_HTML = `<div class="emotion-page" id="emotion_page_view">                    
                     <label id="emotion-box-title" class="form-label">EMOTIONS (*)</label>
                     <hr>
@@ -136,6 +124,17 @@ function Entity_Emotion_Page() {
     Set_Entity_Emotion_Values()
 }
 
+//will take the current emotion values, and store it into an object to replace the current entity object's emotions
+//then update the record in the Database
+function Save_Entity_Emotions() {
+    happy_value = document.getElementById('happy').value
+    sad_value = document.getElementById('sad').value
+    confused_value = document.getElementById('confused').value
+    current_entity_obj.entityEmotions = {happy:happy_value,sad:sad_value,confused:confused_value}
+    ENTITY_DB_FNS.Update_Record(current_entity_obj)
+}
+
+//set the HTML values for the emotion sliders using the current entity object
 function Set_Entity_Emotion_Values() {
     emotion_set_obj = current_entity_obj.entityEmotions
     for (var emotion in emotion_set_obj) {       
@@ -143,43 +142,35 @@ function Set_Entity_Emotion_Values() {
     }
 }
 
+//called from the entity-main.html
 async function New_Entity_Image(){
-
+    
     result = await IPC_RENDERER_PICS.invoke('dialog:openEntity')
     file_tmp = result.filePaths
     if(file_tmp.length > 0){
 
         directory_of_image = PATH.dirname(result.filePaths[0])
         if(directory_of_image != DIR_PICS){//DIR_PICS
-            new_filename = MY_FILE_HELPER.Copy_Non_Taga_Files(result,DIR_PICS)
+            //must pass a string as a filename and not an array which this function returns
+            new_filename = MY_FILE_HELPER.Copy_Non_Taga_Files(result,DIR_PICS)[0]
         } else{
             new_filename = PATH.parse(file_tmp[0]).base
         }
 
-        filename = new_filename//PATH.parse(file_tmp[0]).base
-        current_entity_obj.entityImage = filename
-        default_img = __dirname.substring(0, __dirname.lastIndexOf('/')) + '/images/' + current_entity_obj.entityImage
-        document.getElementById("entityProfileImg").src = default_img//current_entity_obj.entityImage;
-        //include new image in the gallery image set
+        current_entity_obj.entityImage = new_filename
+        default_img = DIR_PICS + '/' + current_entity_obj.entityImage
+        document.getElementById("entityProfileImg").src = default_img
+        //include new image in the gallery image set if not already there
         image_set = current_entity_obj.entityImageSet
         if(image_set.includes(current_entity_obj.entityImage) == false){
             image_set.push(current_entity_obj.entityImage)
             current_entity_obj.entityImageSet = image_set
-            gallery_html = `<div class="row">`
-            gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="Add_Gallery_Images()">add more images</button><br>`
-            gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="New_Gallery_Images()">new images</button><br>`
-            default_PATH = __dirname.substring(0, __dirname.lastIndexOf('/')) + '/images/'
-            image_set = current_entity_obj.entityImageSet
-            image_set.forEach(element => {
-            gallery_html += `
-            <img class="imgG" src="${default_PATH + element}">
-            `
-            });
-            gallery_html += `</div>`
-            document.getElementById("entityGallery").innerHTML  = gallery_html;
 
+        } else {
+            //console.log('IN THE IMAGE SET ALREADY!!!!!')
         }
         ENTITY_DB_FNS.Update_Record(current_entity_obj)
+        Show_Entity_From_Key_Or_Current_Entity(all_entity_keys[current_key_index],0)
     }
 }
 
@@ -198,27 +189,17 @@ async function New_Gallery_Images(){
         if(directory_of_image != DIR_PICS){//DIR_PICS
             files_tmp_base = MY_FILE_HELPER.Copy_Non_Taga_Files(result,DIR_PICS)
         } else{
-            console.log('files are in the taga images directory')
+            //console.log('files are in the taga images directory')
         }
 
         if(files_tmp_base.includes(current_entity_obj.entityImage) == false){
             files_tmp_base.push(current_entity_obj.entityImage)
         }
     }
+
     current_entity_obj.entityImageSet = files_tmp_base
-    gallery_html = `<div class="row">`
-    gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="Add_Gallery_Images()">add more images</button><br>`
-    gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="New_Gallery_Images()">new images</button><br>`
-    default_PATH = __dirname.substring(0, __dirname.lastIndexOf('/')) + '/images/' 
-    image_set = current_entity_obj.entityImageSet
-    image_set.forEach(element => {
-        gallery_html += `
-        <img class="imgG" src="${default_PATH + element}">
-        `
-    });    
-    gallery_html += `</div>`
-    document.getElementById("entityGallery").innerHTML  = gallery_html;
     ENTITY_DB_FNS.Update_Record(current_entity_obj)
+    Show_Entity_From_Key_Or_Current_Entity(all_entity_keys[current_key_index],0)
 
 }
 
@@ -232,11 +213,8 @@ async function Add_Gallery_Images(){
         return PATH.parse(filePATH).base
     })
 
-    if(files_tmp_base.length == 0){
-        console.log('empty gallery array chosen')
-    } else {
-        console.log('non empty gallery array chosen')
-        
+    if(files_tmp.length != 0){ //if nothing to add do nothing
+
         directory_of_image = PATH.dirname(result.filePaths[0])
         if(directory_of_image != DIR_PICS){//DIR_PICS
             console.log('files are not in the taga images directory')
@@ -255,32 +233,21 @@ async function Add_Gallery_Images(){
                 }
             })
         }
-
         if(files_tmp_base.includes(current_entity_obj.entityImage) == false){
             files_tmp_base.push(current_entity_obj.entityImage)
         }
+        current_entity_obj.entityImageSet = image_set_tmp
+
+        ENTITY_DB_FNS.Update_Record(current_entity_obj)
+        Show_Entity_From_Key_Or_Current_Entity(all_entity_keys[current_key_index],0)
     }
-
-    current_entity_obj.entityImageSet = image_set_tmp
-    gallery_html = `<div class="row">`
-    gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="Add_Gallery_Images()">add more images</button><br>`
-    gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="New_Gallery_Images()">new images</button><br>`
-    default_PATH = __dirname.substring(0, __dirname.lastIndexOf('/')) + '/images/' 
-    image_set = current_entity_obj.entityImageSet
-    image_set.forEach(element => {
-        gallery_html += `
-        <img class="imgG" src="${default_PATH + element}">
-        `
-    });    
-    gallery_html += `</div>`
-    document.getElementById("entityGallery").innerHTML  = gallery_html;
-    ENTITY_DB_FNS.Update_Record(current_entity_obj)
-
 }
 
-async function Show_Entity_From_Key(entity_key) {
+async function Show_Entity_From_Key_Or_Current_Entity(entity_key_or_obj,use_key=1) {
 
-    current_entity_obj = await ENTITY_DB_FNS.Get_Record(entity_key) 
+    if(use_key == 1){
+        current_entity_obj = await ENTITY_DB_FNS.Get_Record(entity_key_or_obj) 
+    } 
     //entity name
     document.getElementById("entityName").textContent = '#' + current_entity_obj.entityName;
     //entity profile image
@@ -316,7 +283,7 @@ function Prev_Image() {
     } else {
         current_key_index = all_entity_keys.length-1
     }
-    Show_Entity_From_Key(all_entity_keys[current_key_index])
+    Show_Entity_From_Key_Or_Current_Entity(all_entity_keys[current_key_index])
 }
 
 async function Next_Image() {
@@ -325,7 +292,7 @@ async function Next_Image() {
     } else {
         current_key_index = 0
     }
-    Show_Entity_From_Key(all_entity_keys[current_key_index])
+    Show_Entity_From_Key_Or_Current_Entity(all_entity_keys[current_key_index])
     
     
 }
@@ -335,8 +302,8 @@ async function Initialize_Entity_Page(){
     await ENTITY_DB_FNS.Get_All_Keys_From_DB() //gets all entity keys, sets them as a variable available for access later on
     all_entity_keys = ENTITY_DB_FNS.Read_All_Keys_From_DB() //retrieve the key set stored as a global within the module
 
-    await Show_Entity_From_Key(all_entity_keys[0]) //set the first entity to be seen, populate entity object data on view
-    await Entity_Emotion_Page() //the entity annotation
+    await Show_Entity_From_Key_Or_Current_Entity(all_entity_keys[0]) //set the first entity to be seen, populate entity object data on view
+    await Entity_Emotion_Page() //the entity annotation is the first page to see alternative is the text description
     
 }
 
@@ -345,6 +312,53 @@ Initialize_Entity_Page()
 
 
 
+
+
+
+/*
+    gallery_html = `<div class="row">`
+    gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="Add_Gallery_Images()">add more images</button><br>`
+    gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="New_Gallery_Images()">new images</button><br>`
+    default_PATH = __dirname.substring(0, __dirname.lastIndexOf('/')) + '/images/' 
+    image_set = current_entity_obj.entityImageSet
+    image_set.forEach(element => {
+        gallery_html += `
+        <img class="imgG" src="${default_PATH + element}">
+        `
+    });    
+    gallery_html += `</div>`
+    document.getElementById("entityGallery").innerHTML  = gallery_html;
+*/
+
+/*
+    gallery_html = `<div class="row">`
+    gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="Add_Gallery_Images()">add more images</button><br>`
+    gallery_html += `<button type="button" class="btn btn-primary btn-lg" onclick="New_Gallery_Images()">new images</button><br>`
+    default_PATH = __dirname.substring(0, __dirname.lastIndexOf('/')) + '/images/' 
+    image_set = current_entity_obj.entityImageSet
+    image_set.forEach(element => {
+        gallery_html += `
+        <img class="imgG" src="${default_PATH + element}">
+        `
+    });    
+    gallery_html += `</div>`
+    document.getElementById("entityGallery").innerHTML  = gallery_html;
+    ENTITY_DB_FNS.Update_Record(current_entity_obj)
+*/
+
+/*
+    gallery_html = `<div class="row" id="meme_page_view">`
+    memes_array = current_entity_obj.entityMemes
+    if(memes_array != ""){
+        memes_array.forEach(element => {
+            gallery_html += `
+            <img class="imgG" src="${DIR_PICS + '/' + element}">
+            `
+        });    
+    }
+    gallery_html += `<br><button type="button" class="btn btn-primary btn-lg" onclick="New_Entity_Memes()">Choose new memes</button>`
+    document.getElementById("annotationPages").innerHTML  = gallery_html;
+    */
 
 /*
 Load_First_Image()
--- AppCode/myJS/copy-new-file-helper.js
+++ AppCode/myJS/copy-new-file-helper.js
@@ -1,3 +1,5 @@
+const PATH = require('path');
+const FS = require('fs');
 
 
 //console.log(makeid(5));
@@ -19,22 +21,22 @@ function Copy_Non_Taga_Files(result,dir_pics){
     new_filename_array = []
     console.log(`array length of the file paths: ${result.filePaths.length}`)
     result.filePaths.forEach(filepath_tmp => {
-        filename = path.parse(filepath_tmp).base;
+        filename = PATH.parse(filepath_tmp).base;
         filename_path_to_local = `${dir_pics}/${filename}`
         console.log(filename_path_to_local)
         
-        if(fs.existsSync(filename_path_to_local)) {
+        if(FS.existsSync(filename_path_to_local)) {
             console.log('path DOES EXIT for filename locally')
             // path exists
             //ADD SALT and use the SALTY image ref varname = salt_fn
             salt_tmp = Make_Salt(10)
-            new_filename = path.parse(filename).name + salt_tmp + path.parse(filename).ext
+            new_filename = PATH.parse(filename).name + salt_tmp + PATH.parse(filename).ext
             console.log(new_filename)
-            fs.copyFileSync(filepath_tmp, `${dir_pics}/${new_filename}`, fs.constants.COPYFILE_EXCL)
+            FS.copyFileSync(filepath_tmp, `${dir_pics}/${new_filename}`, FS.constants.COPYFILE_EXCL)
             new_filename_array.push(new_filename)
         } else{
             console.log('path DOES NOT EXIST for filename locally')
-            fs.copyFileSync(filepath_tmp, `${dir_pics}/${filename}`, fs.constants.COPYFILE_EXCL)
+            FS.copyFileSync(filepath_tmp, `${dir_pics}/${filename}`, FS.constants.COPYFILE_EXCL)
             new_filename_array.push(filename)
         }
     });
