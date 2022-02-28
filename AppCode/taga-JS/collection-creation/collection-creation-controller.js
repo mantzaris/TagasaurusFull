e012815a0085b6c397dffe5be3608114e8daa03e
mantzaris <avmantzaris@gmail.com>
2022-02-28 00:07:35 -0500

    now the basic step1 unique name check for the collection DB is there

--- AppCode/taga-JS/collection-creation/collection-creation-controller.js
+++ AppCode/taga-JS/collection-creation/collection-creation-controller.js
@@ -1,9 +1,15 @@
 
 
 
+const COLLECTION_DB_MODULE = require('./myJS/entity-db-fns.js');
 
 
-
+async function Create_Collection_DB_Instance() {
+    await COLLECTION_DB_MODULE.Create_Db()
+}
+async function Get_Collection_Record_In_DB(entity_key) {
+    return await COLLECTION_DB_MODULE.Get_Record(entity_key)
+}
 
 
 var creation_step_num = 1
@@ -57,7 +63,6 @@ function Navbar_ViewHandle() {
         nav_btn5.classList.remove('nav-bar-off')
         nav_btn5.classList.add('nav-bar-on')
     }
-
 }
 function Creation_Back_Btn() {
     if(creation_step_num > 1) {
@@ -73,14 +78,25 @@ function Creation_Back_Btn() {
     }
     Navbar_ViewHandle()
 }
-function Creation_Next_Btn() {
+async function Creation_Next_Btn() {
+    //check to see if the user has completed the required components or not, if not return
+    if(creation_step_num == 1) {
+        collection_name_text_area = document.getElementById("collection-name-textarea-id").value
+        collection_obj = await Get_Collection_Record_In_DB(collection_name_text_area)
+        if(collection_obj != undefined) {
+            return //exit without proceeding until unique name supplied
+        }
+    }
+    //update the counter
     if(creation_step_num < 5) {
         creation_step_num += 1
     }
+    //the invisible back button make visible cause now it is possible to go back from step 2
     if(creation_step_num == 2){
         button_back = document.getElementById("creation-back-button-id")
         button_back.style.display = "block"
     }
+    //at the end and notify the user that they can now complete the creation steps
     if(creation_step_num == 5){
         button_back = document.getElementById("creation-next-button-id")
         button_back.innerHTML = "COMPLETE"
@@ -90,8 +106,9 @@ function Creation_Next_Btn() {
 
 
 
-function Initialize_Collection_Creation_Page() {
-
+async function Initialize_Collection_Creation_Page() {
+    //set up the nav bar and bottom buttons
+    Navbar_ViewHandle()
     
 
     document.getElementById("creation-back-button-id").addEventListener("click", function (event) {
@@ -100,10 +117,10 @@ function Initialize_Collection_Creation_Page() {
     document.getElementById("creation-next-button-id").addEventListener("click", function (event) {
         Creation_Next_Btn()
     })
+    //init the collection DB
+    await Create_Collection_DB_Instance()
 
 
-
-    
 
 }
 //the key starting point for the page>>>>>>>>>>>>
