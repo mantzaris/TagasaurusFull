727624e004c179e79cccc2d6999b6f7d0dbc4dff
mantzaris <avmantzaris@gmail.com>
2022-12-16 22:28:42 -0500

    now the basic stream search is working and there is a potential issue if the face move and the face fills a box belonging to another face it can swap the key words

--- AppCode/taga-JS/stream-search/stream-search.js
+++ AppCode/taga-JS/stream-search/stream-search.js
@@ -78,6 +78,7 @@ return_from_stream_btn.onclick = () => {
     stream_ok = false
     clearInterval(face_keywords_interval)
     clearInterval(descriptors_interval)
+    clearInterval(memory_loss_interval)
     stream.getTracks().forEach(function(track) {
         track.stop();
     });
@@ -143,7 +144,6 @@ async function Stream_Search_Run() {
         photo.setAttribute('src', data);
     }
 
-    //GenerateTable() //generate the table to view the keywords !!! replace
     //START: loop for the face detection and descriptors, runs in its own interval, supplies coordinates for the canvas drawing and descriptors for the db searching
     const fps = 3 //how often we run the face detection
     let rect_face_array = [] //storing the each face detected for that run of the face detection api (rewrites itself each time to be fresh)
@@ -167,6 +167,7 @@ async function Stream_Search_Run() {
 
     //START: loop for drawing the boxes of the face rectangles on the canvas, selected box has a different stroke style
     let face_found = false //if key
+    let outline_face_not_in_focus = false //don't outline and highlight faces that are not being investigated
     let rect_face_selected = { x:0, y:0, width:0, height:0 } //holds the selected face which descriptors focus on in this cycle
     async function Draw_Descriptors() {        
         if( canvas.width > 0 && canvas.height > 0 ) {
@@ -192,14 +193,16 @@ async function Stream_Search_Run() {
                 ctx.stroke()                
             }
             //draw boxes around the rest of the faces in dashes OPTIONAL can be turned off as well
-            for( const [ind,face_rect] of rect_face_array.entries() ) { //draw rest of the faces not in selected
-                if(selected_face_ind == ind) { continue } //ignore the selected face so that it stays as solid stroke and the rest as dashed
-                ctx.beginPath()
-                ctx.rect(face_rect.x,face_rect.y,face_rect.width,face_rect.height)
-                ctx.strokeStyle = 'red'
-                ctx.setLineDash([16, 14]); //dashes are 5px and spaces are 3px
-                ctx.lineWidth = 3
-                ctx.stroke()  
+            if( outline_face_not_in_focus ) {
+                for( const [ind,face_rect] of rect_face_array.entries() ) { //draw rest of the faces not in selected
+                    if(selected_face_ind == ind) { continue } //ignore the selected face so that it stays as solid stroke and the rest as dashed
+                    ctx.beginPath()
+                    ctx.rect(face_rect.x,face_rect.y,face_rect.width,face_rect.height)
+                    ctx.strokeStyle = 'red'
+                    ctx.setLineDash([16, 14]); //dashes are 5px and spaces are 3px
+                    ctx.lineWidth = 3
+                    ctx.stroke()  
+                }
             }
         } else { }
         //place the keywords for the user to see
@@ -210,7 +213,7 @@ async function Stream_Search_Run() {
     }
     function Display_Keywords() {
         keyword_div.innerHTML = ""
-        keywords_html = ""
+        keywords_html = "Keywords: <br>"
         for( const keyword of face_keywords ) {
             keywords_html += `
                             <div class="keyword">
@@ -223,7 +226,7 @@ async function Stream_Search_Run() {
     //END: loop for drawing the boxes of the face rectangles on the canvas, selected box has a different stroke style
 
     //START: in a separate loop, search and fill keywords for a randomly selected face in the faces array rect_face_array (next version should use tree based indexes on clusters)
-    let db_search_delay = 1000 //amount of time the DB has to find data related to a face, in ms
+    let db_search_delay = 2500 //amount of time the DB has to find data related to a face, in ms
     let max_records = await Number_of_Tagging_Records() 
     let record_sample_num = Math.min( 5000 , Math.floor( 1 * max_records ) ) //allow up to 4K records to be searched in each sweep since more may incur too much delay
     let face_keywords = [] //holds the keywords for the selected image in focus
@@ -244,6 +247,9 @@ async function Stream_Search_Run() {
                 //console.log(face_keywords)
             }        
         }
+        //check for the face being recently seen that can ADD to the new just discovered keywords (updates the keywords directly)
+        await Face_Keywords_Short_Memory(ref_face_tmp_descriptor)
+
         console.log(face_keywords)
         if( face_keywords.length > 0 ) { //face matches were found
             face_found = true             
@@ -260,25 +266,54 @@ async function Stream_Search_Run() {
             rect_face_selected.width = ref_face_tmp.width
             rect_face_selected.height = ref_face_tmp.height
         }
-        //tableFiller( face_keywords )
     }
     face_keywords_interval = setInterval( face_db_search , db_search_delay )
     //END: in a separate loop, search and fill keywords for a random face in the face array (next version should use tree based indexes on clusters)
 
-
+    //START: memory of the keywords so that if a face disappears and comes back the keywords are remembered for some time 
+    //related to that face descriptor, also helpes when the face box is cycling through a set
+    let faces_short_memories = [] //hold objects of face-descriptors-and-counters-keywords to remember face
+    let memory_time = 1000*60*2
+    let face_memory = {memory_time:memory_time, descriptor:[], keywords:[]}
+    async function Face_Keywords_Short_Memory(ref_face_descriptor) {
+        if( faces_short_memories.length == 0 ) { //keywords not updated since this is a first to be seen
+            let new_face_memory = JSON.parse(JSON.stringify(face_memory))
+            new_face_memory.descriptor = ref_face_descriptor
+            new_face_memory.keywords = face_keywords
+            faces_short_memories.push(new_face_memory)
+            return //nothing left to do so exit
+        }
+        let found_face = false
+        for( const [ind,memory] of faces_short_memories.entries() ) { //loop through memories to see if this descriptor is present or not
+            let score_tmp = await Get_Descriptors_DistanceScore( [ref_face_descriptor] , [memory.descriptor] ) 
+            if( score_tmp > 5 ) { //found a similar face so no new inclusion and accumulate keywords now and in memory
+                found_face = true
+                faces_short_memories[ind].memory_time = memory_time //refresh memory
+                faces_short_memories[ind].keywords.push(...face_keywords) //merge the arrays
+                faces_short_memories[ind].keywords = [...new Set(faces_short_memories[ind].keywords)] //get the unique of the merged
+                face_keywords = faces_short_memories[ind].keywords //update the current keyword list
+            }
+        }
+        if(found_face == false) { //face needs to be added to the seen set
+            let new_face_memory = JSON.parse(JSON.stringify(face_memory))
+            new_face_memory.memory_time = memory_time
+            new_face_memory.keywords = face_keywords
+            new_face_memory.descriptor = ref_face_descriptor
+            faces_short_memories.push(new_face_memory)
+        }
+    }
+    function Face_Short_Memory_Reduction() { //reduce the time for each memory on a loop and remove those out of time
+        for( const [ind,memory] of faces_short_memories.entries() ) { 
+            console.log({faces_short_memories})
+            faces_short_memories[ind].memory_time -= 10000 //take time off of that memory
+            console.log({faces_short_memories})
+        }
+        faces_short_memories = faces_short_memories.filter( (memory,i) => memory.memory_time > 0 ) //keep those with memory time
+    }
+    memory_loss_interval = setInterval( Face_Short_Memory_Reduction, 15000)
 
 
 }
+//END of stream search functionality view
 
 
-
-
-    //document.getElementById("stream-view1").style.display = 'block'
-
-
-    // //Init()
-    // async function f1() {
-    //     stream_ok = await Set_Stream()
-    //     if(!stream_ok) { return }
-    //     Stream_Search_Run()   }
-    // f1()
