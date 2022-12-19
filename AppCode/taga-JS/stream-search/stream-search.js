e04822a56f5b3bd269fd6e603870ffe40400d75d
mantzaris <avmantzaris@gmail.com>
2022-12-18 21:08:13 -0500

    now the stream search with keywords and images is working in testing

--- AppCode/taga-JS/stream-search/stream-search.js
+++ AppCode/taga-JS/stream-search/stream-search.js
@@ -7,14 +7,20 @@ const keywords_images_description = "Displays keywords and images related to fac
 const keywords_images_memes_description = "Displays keywords, images and memes related to faces stored. Finds images/videos/gifs containing a similar face and displays the keywords from the description as well as images containing the face with a match. Connected memes will also be presented."
 
 document.getElementById("stream-view1").style.display = 'none'
-const continue_btn = document.getElementById("continue-btn")
+document.getElementById("stream-view2").style.display = 'none'
+
+const webcam_selection_btn = document.getElementById("continue-btn")
 const main_menu_btn = document.getElementById("main-menu-btn")
 const selection_set = document.getElementById("search-type")
 const selection_description = document.getElementById("stream-type-description")
-const return_from_stream_btn = document.getElementById("return-stream-btn")
+const return_from_stream_btn1 = document.getElementById("return-stream-btn1")
+return_from_stream_btn1.onclick = () => { Stop_Stream_Search() }
+const return_from_stream_btn2 = document.getElementById("return-stream-btn2")
+return_from_stream_btn2.onclick = () => { Stop_Stream_Search() }
+
+let stream_selection = "keywords-only"
 selection_description.innerText = keywords_only_description
 selection_set.onchange = () => {
-    console.log("new selection")
     console.log(`new selection is = ${selection_set.value}`)
     if( selection_set.value == "keywords-only" ) {
         selection_description.innerText = keywords_only_description
@@ -23,10 +29,11 @@ selection_set.onchange = () => {
     } else if( selection_set.value == "keywords-images-memes" ) {
         selection_description.innerText = keywords_images_memes_description
     } 
+    stream_selection = selection_set.value
 }
 
 
-const { DB_MODULE, MAX_COUNT_SEARCH_RESULTS, SEARCH_MODULE, GENERAL_HELPER_FNS } = require(PATH.join(__dirname,'..','constants','constants-code.js')) // require(PATH.resolve()+PATH.sep+'constants'+PATH.sep+'constants-code.js');
+const { DB_MODULE, TAGA_DATA_DIRECTORY, GENERAL_HELPER_FNS } = require(PATH.join(__dirname,'..','constants','constants-code.js')) // require(PATH.resolve()+PATH.sep+'constants'+PATH.sep+'constants-code.js');
 async function Tagging_Image_DB_Iterator() {
     return DB_MODULE.Tagging_Image_DB_Iterator();
 }
@@ -41,7 +48,18 @@ async function Number_of_Tagging_Records() {
 }
 
 
-continue_btn.onclick = async () => {
+//START: INIT STUFF 
+let stream
+let video
+let stream_ok = false
+let descriptors_interval
+let face_keywords_interval
+let keyword_div
+let images_div
+let canvas
+
+
+webcam_selection_btn.onclick = async () => {
     Init()
     let stream_ok = await Set_Stream()
     if(!stream_ok) { return }
@@ -52,29 +70,55 @@ main_menu_btn.onclick = () => {
     location.href = "welcome-screen.html";
 }
 
-function Keywords_Only() {
+function Keywords_Only_Start() {
+    document.getElementById("selection-screen").style.display = 'none'
+    document.getElementById("stream-view1").style.display = 'grid'
+    video = document.getElementById("inputVideo1")
+    keyword_div = document.getElementById("keyword-display1-div")
+    canvas = document.getElementById("overlay1")
 }
-function Keywords_Images() {
+function Keywords_Images_Start() {
+    document.getElementById("selection-screen").style.display = 'none'
+    document.getElementById("stream-view2").style.display = 'grid'
+    video = document.getElementById("inputVideo2")
+    keyword_div = document.getElementById("keyword-display2-div")
+    images_div = document.getElementById("images-display2-div")
+    canvas = document.getElementById("overlay2")
 }
-function Keywords_Images_Memes() {
+function Keywords_Images_Memes_Start() {
+}
+function Keywords_Only_End() {
+    document.getElementById("stream-view1").style.display = 'none'
+    document.getElementById("selection-screen").style.display = 'block'
+}
+function Keywords_Images_End() {
+    document.getElementById("stream-view2").style.display = 'none'
+    document.getElementById("selection-screen").style.display = 'block'
+}
+function Keywords_Images_Memes_End() {
 }
 
 function Init() {
     let selection = selection_set.value
     if( selection == "keywords-only" ) {
-        Keywords_Only()
+        Keywords_Only_Start()
     } else if ( selection == "keywords-images" ) {
-        Keywords_Images()
+        Keywords_Images_Start()
     } else {
-        Keywords_Images_Memes()
+        Keywords_Images_Memes_Start()
     }
-    document.getElementById("selection-screen").style.display = 'none'
-    document.getElementById("stream-view1").style.display = 'grid'
+    
 }
 //user returns to stream search menu from the running search stream and needs to change the view and stop the stream
-return_from_stream_btn.onclick = () => { 
-    document.getElementById("stream-view1").style.display = 'none'
-    document.getElementById("selection-screen").style.display = 'block'
+function Stop_Stream_Search() { 
+    let selection = selection_set.value
+    if( selection == "keywords-only" ) {
+        Keywords_Only_End()
+    } else if ( selection == "keywords-images" ) {
+        Keywords_Images_End()
+    } else {
+        Keywords_Images_Memes_End()
+    }    
     stream_ok = false
     clearInterval(face_keywords_interval)
     clearInterval(descriptors_interval)
@@ -83,13 +127,10 @@ return_from_stream_btn.onclick = () => {
         track.stop();
     });
 }
+//END: INIT STUFF 
 
 
-let stream
-let video = document.getElementById("inputVideo")
-let stream_ok = false
-let descriptors_interval
-let face_keywords_interval
+
 
 
 async function Set_Stream() {
@@ -112,12 +153,9 @@ async function Stream_Search_Run() {
     let width
     let height
     let data    
-    let keyword_div = document.getElementById("keyword-display1-div")
 
     video.style.display = 'none'
     
-    let canvas = document.getElementById("overlay")
-    // canvas.style.display = 'block'
     const ctx = canvas.getContext('2d');
     let photo = document.createElement('img')    
 
@@ -206,9 +244,21 @@ async function Stream_Search_Run() {
             }
         } else { }
         //place the keywords for the user to see
-        if( face_keywords.length > 0 && face_found == true ) {
-            Display_Keywords()
-        }
+
+        if( stream_selection == "keywords-only" ) {
+            if( face_keywords.length > 0 && face_found == true ) {
+                Display_Keywords()
+            }
+        } else if ( stream_selection == "keywords-images" ) {
+            if( face_keywords.length > 0 && face_found == true ) {
+                Display_Keywords()
+            }
+            if( face_including_images.length > 0 && face_found == true ) {
+                Display_Images_Found()
+            }
+        } else {
+            
+        }    
         if(stream_ok) { requestAnimationFrame(Draw_Descriptors) }
     }
     function Display_Keywords() {
@@ -223,6 +273,19 @@ async function Stream_Search_Run() {
         }
         keyword_div.innerHTML = keywords_html
     }
+    function Display_Images_Found() {
+        images_div.innerHTML = ""
+        images_html = "Images: <br>"
+        for( const image of face_including_images ) {
+            //console.log(`face image = ${image}`)
+            images_html += `
+                            <div class="image-thumbnail-div">
+                                <img class="image-thumbnail" id="" src="${TAGA_DATA_DIRECTORY}${PATH.sep}${image}" title="view" alt="meme" />
+                            </div>
+                            `
+        }
+        images_div.innerHTML = images_html
+    }
     //END: loop for drawing the boxes of the face rectangles on the canvas, selected box has a different stroke style
 
     //START: in a separate loop, search and fill keywords for a randomly selected face in the faces array rect_face_array (next version should use tree based indexes on clusters)
@@ -230,8 +293,10 @@ async function Stream_Search_Run() {
     let max_records = await Number_of_Tagging_Records() 
     let record_sample_num = Math.min( 5000 , Math.floor( 1 * max_records ) ) //allow up to 4K records to be searched in each sweep since more may incur too much delay
     let face_keywords = [] //holds the keywords for the selected image in focus
+    let face_including_images = [] //holds the images where a face match was found (could be 1 face in many present)
     async function face_db_search() { //will improve in next version !!!! XXX use tree based methods!!!
         face_keywords = [] //reset the keywords
+        face_including_images = [] //reset the face images
         if( rect_face_array.length == 0 ) { return } //if no faces present skip/exit
         let ref_face_tmp = rect_face_array[ Math.floor(Math.random()*rect_face_array.length) ] //get some random face to analyze
         let ref_face_tmp_descriptor = ref_face_tmp.descriptor //get the face descriptor array
@@ -244,11 +309,12 @@ async function Stream_Search_Run() {
             //console.log(`score_tmp = ${score_tmp}`)
             if( score_tmp > 5 ) { //why is this not between 0 and 1 ??? !!!! xxx
                 face_keywords.push(...annotation_obj_tmp["taggingTags"]) //add to the keywords
+                face_including_images.push(annotation_obj_tmp["fileName"])
                 //console.log(face_keywords)
             }        
         }
         //check for the face being recently seen that can ADD to the new just discovered keywords (updates the keywords directly)
-        await Face_Keywords_Short_Memory(ref_face_tmp_descriptor)
+        await Faces_Short_Memory(ref_face_tmp_descriptor)
 
         console.log(face_keywords)
         if( face_keywords.length > 0 ) { //face matches were found
@@ -260,7 +326,7 @@ async function Stream_Search_Run() {
         }
         else { //face matches were not found but we still notify the user that it is not found
             face_found = true//false
-            face_keywords.push(...['nothing found yet']) //notify the user that nothing was found yet
+            //face_keywords.push(...['nothing found yet']) //notify the user that nothing was found yet
             rect_face_selected.x = ref_face_tmp.x
             rect_face_selected.y = ref_face_tmp.y
             rect_face_selected.width = ref_face_tmp.width
@@ -274,30 +340,35 @@ async function Stream_Search_Run() {
     //related to that face descriptor, also helpes when the face box is cycling through a set
     let faces_short_memories = [] //hold objects of face-descriptors-and-counters-keywords to remember face
     let memory_time = 1000*60*2
-    let face_memory = {memory_time:memory_time, descriptor:[], keywords:[]}
-    async function Face_Keywords_Short_Memory(ref_face_descriptor) {
+    let face_memory = {memory_time:memory_time, descriptor:[], keywords:[], images:[]}
+    async function Faces_Short_Memory(ref_face_descriptor) {
         if( faces_short_memories.length == 0 ) { //keywords not updated since this is a first to be seen
             let new_face_memory = JSON.parse(JSON.stringify(face_memory))
             new_face_memory.descriptor = ref_face_descriptor
             new_face_memory.keywords = face_keywords
+            new_face_memory.images = face_including_images
             faces_short_memories.push(new_face_memory)
             return //nothing left to do so exit
         }
         let found_face = false
         for( const [ind,memory] of faces_short_memories.entries() ) { //loop through memories to see if this descriptor is present or not
-            let score_tmp = await Get_Descriptors_DistanceScore( [ref_face_descriptor] , [memory.descriptor] ) 
+            let score_tmp = await Get_Descriptors_DistanceScore( [ref_face_descriptor] , [memory.descriptor] )
             if( score_tmp > 5 ) { //found a similar face so no new inclusion and accumulate keywords now and in memory
                 found_face = true
                 faces_short_memories[ind].memory_time = memory_time //refresh memory
                 faces_short_memories[ind].keywords.push(...face_keywords) //merge the arrays
                 faces_short_memories[ind].keywords = [...new Set(faces_short_memories[ind].keywords)] //get the unique of the merged
                 face_keywords = faces_short_memories[ind].keywords //update the current keyword list
+                faces_short_memories[ind].images.push(...face_including_images) //merge the arrays
+                faces_short_memories[ind].images = [...new Set(faces_short_memories[ind].images)] //get the unique of the merged
+                face_including_images = faces_short_memories[ind].images //update the current images list
             }
         }
         if(found_face == false) { //face needs to be added to the seen set
             let new_face_memory = JSON.parse(JSON.stringify(face_memory))
             new_face_memory.memory_time = memory_time
             new_face_memory.keywords = face_keywords
+            new_face_memory.images = face_including_images
             new_face_memory.descriptor = ref_face_descriptor
             faces_short_memories.push(new_face_memory)
         }
@@ -311,9 +382,16 @@ async function Stream_Search_Run() {
         faces_short_memories = faces_short_memories.filter( (memory,i) => memory.memory_time > 0 ) //keep those with memory time
     }
     memory_loss_interval = setInterval( Face_Short_Memory_Reduction, 15000)
-
+    //END: memory 
 
 }
 //END of stream search functionality view
 
 
+//     let ft_res = await fileType.fromFile( PATH.join(TAGA_DATA_DIRECTORY, current_image_annotation["fileName"]) )
+    //console.log('ft_res = ', ft_res)
+//    if( ft_res.mime.includes('image') == true ) {
+    //const fileType = require('file-type');
+//    const display_path = `${TAGA_DATA_DIRECTORY}${PATH.sep}${current_image_annotation["fileName"]}`
+//src="${TAGA_DATA_DIRECTORY}${PATH.sep}${file}" 
+//                content_html = `<img class="memes-img-class" id="memes-image-img-id-${file}" src="${TAGA_DATA_DIRECTORY}${PATH.sep}${file}" title="view" alt="meme" />`
