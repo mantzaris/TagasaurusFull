8c4985fec22e809382bca71c7d8698ae2b502754
mantzaris <avmantzaris@gmail.com>
2023-08-11 00:26:22 -0400

    finished the bash script for the linux zip so that it unzips and rezips the files and looks for the linux builds of zip and makes sure the bash script dir is in the root

--- tgdev.sh
+++ tgdev.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 BUILD_INSTALLER=true
 
+# Check dependencies
+check_dependency() {
+    command -v "$1" >/dev/null 2>&1 || { echo >&2 "$1 is required but it's not installed. Aborting."; exit 1; }
+}
+
+check_dependency unzip
+check_dependency zip
+check_dependency npm
+check_dependency npx
+#########
+
 set_config(){
     echo "{ \"BUILD_INSTALLER\": $BUILD_INSTALLER }" > config.json
 }
 
 if [ "$1" == "run" ]; then
+
     echo "running development build"
     BUILD_INSTALLER=false
     set_config
     npx cross-env OSTARGETS="--linux=deb rpm" npm run dev
     exit 0
+
 elif [ "$1" == "pack" ]; then
 
     echo "cleaning dependencies"
@@ -19,13 +32,31 @@ elif [ "$1" == "pack" ]; then
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
         echo "building linux installers"
-        #set_config
-        #npx cross-env OSTARGETS="--linux=deb rpm" npm run build
+        set_config
+        npx cross-env OSTARGETS="--linux=deb rpm" npm run build
         
         echo "building linux zip"
         BUILD_INSTALLER=false
         set_config
         npx cross-env OSTARGETS="--linux=zip" npm run build
+
+        # Unzip the build and zip again after the unpacked directory holding the LinuxRunOnExternalMedia for the USB remount script, is moved to the app root directory for the user to access easily
+        for zip_file in dist/tagasaurus-*.zip; do
+            # Extract the version or unique identifier from the filename
+            identifier=$(basename "$zip_file" .zip | cut -d'-' -f2)
+            # Extract the ZIP file
+            unzip "$zip_file" -d dist/tempDir || { echo "Failed to unzip $zip_file."; exit 1; }
+            # Copy the directory within the extracted contents
+            cp -r dist/tempDir/resources/app.asar.unpacked/LinuxRunOnExternalMedia dist/tempDir/LinuxRunOnExternalMedia
+            # Recreate the ZIP file with the modified contents
+            (
+                cd dist/tempDir || exit
+                zip -r "../tagasaurus-$identifier-Linux.zip" .
+            )
+            # Clean up: remove the extracted contents and the original ZIP
+            rm -rf dist/tempDir
+            rm "$zip_file"
+        done
 
     elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
 
