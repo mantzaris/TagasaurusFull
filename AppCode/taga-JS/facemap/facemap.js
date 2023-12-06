0fa7d2866661303767e33f69162af30a171e48c0
mantzaris <avmantzaris@gmail.com>
2023-12-05 22:33:52 -0500

    now there is a dropdown menu for the facemaps where clicking on a cluster thumbnail produces a popover list of thumbnails

--- AppCode/taga-JS/facemap/facemap.js
+++ AppCode/taga-JS/facemap/facemap.js
@@ -1,5 +1,5 @@
 const PATH = require('path');
-const { DB_MODULE } = require(PATH.join(__dirname, '..', 'constants', 'constants-code.js'));
+const { DB_MODULE, GENERAL_HELPER_FNS } = require(PATH.join(__dirname, '..', 'constants', 'constants-code.js'));
 const { Full_Path_From_File_Name, Clamp } = require(PATH.join(__dirname, '..', 'AppCode', 'taga-JS', 'utilities', 'general-helper-fns.js'));
 
 const toggle_element = document.getElementById('check1');
@@ -7,6 +7,7 @@ const size_element = document.getElementById('size');
 const container = document.getElementById('mynetwork');
 
 const settings = {
+  show_selected_thumbnails: [],
   show_image_thumbnail: false,
   image_size: 10,
 };
@@ -23,8 +24,34 @@ toggle_element.addEventListener('click', () => {
   Generate_Face_Map(Object.assign(settings));
 });
 
+const id_map = new Map();
+
+function Get_Node_Info_From_ID(id) {
+  for (const val of id_map.values()) {
+    if (val.id == id) return val;
+  }
+}
+
 async function Generate_Face_Map(settings) {
   const all_face_clusters = await DB_MODULE.Get_All_FaceClusters();
+  const all_images = [...new Set(all_face_clusters.flatMap((c) => Object.keys(c.images)))];
+
+  for (const image of all_images) {
+    const id = id_map.has(image) ? id_map.get(image).id : crypto.randomUUID();
+    id_map.set(image, {
+      id,
+      image,
+    });
+  }
+
+  for (const cluster of all_face_clusters) {
+    const id = id_map.has(cluster.rowid) ? id_map.get(cluster.rowid).id : crypto.randomUUID();
+    id_map.set(cluster.rowid, {
+      id,
+      cluster,
+    });
+  }
+
   const face_ids = new Set();
   const face_cluster_ids = all_face_clusters.map((fc) => fc.rowid);
 
@@ -34,8 +61,9 @@ async function Generate_Face_Map(settings) {
   for (const cluster of all_face_clusters) {
     const images = Object.keys(cluster.images);
     const image = images[Math.floor(Math.random() * images.length)];
+
     const node = {
-      id: cluster.rowid,
+      id: id_map.get(cluster.rowid).id,
       label: cluster.rowid,
       shape: 'image',
       size: 20,
@@ -46,25 +74,25 @@ async function Generate_Face_Map(settings) {
 
     const available_edges = face_cluster_ids.filter((fc_id) => fc_id != cluster.rowid);
     const length = 250; // Math.ceil(Math.random() * 75) + 25;
-    const from = cluster.rowid;
+    const from = id_map.get(cluster.rowid).id;
 
     for (const to of available_edges) {
-      face_cluster_edges.push({ from, to, length });
+      face_cluster_edges.push({ from, to: id_map.get(to).id, length });
     }
 
     for (const to of images) {
-      face_cluster_edges.push({ from, to, length: 40 });
+      face_cluster_edges.push({ from, to: id_map.get(to).id, length: 40 });
     }
 
     for (const image of images) {
       const node = {
-        id: image,
+        id: id_map.get(image).id,
         size: settings.image_size,
         shape: 'dot',
         color: '#000',
       };
 
-      if (settings.show_image_thumbnail) {
+      if (settings.show_image_thumbnail || settings.show_selected_thumbnails.includes(image)) {
         node.shape = 'image';
         node.image = Full_Path_From_File_Name(image);
       }
@@ -95,6 +123,73 @@ async function Generate_Face_Map(settings) {
   };
 
   let network = new vis.Network(container, data, options);
+
+  network.on('selectNode', async (ev) => {
+    const node_id = ev.nodes[0];
+    const selected = Get_Node_Info_From_ID(node_id);
+    console.log(selected);
+    if (selected.image) {
+      settings.show_selected_thumbnails = [];
+      GENERAL_HELPER_FNS.Goto_Tagging_Entry(selected.image);
+    }
+
+    if (selected.cluster) {
+      const child_nodes = Object.keys(selected.cluster.images);
+      settings.show_selected_thumbnails = child_nodes;
+      network.storePositions();
+      //await Generate_Face_Map(Object.assign({}, settings));
+      ShowClusterInfoModal(selected, network);
+    }
+  });
+}
+
+function ShowClusterInfoModal({ cluster, id }, network) {
+  const modal = document.getElementById('cluster-modal');
+  document.getElementById('cluster-modal-id').innerText = cluster.rowid;
+  modal.classList.remove('hidden');
+
+  const pos = CanvasToWindowXY(network.getPosition(id), document.querySelector('canvas'));
+  modal.style.left = `${pos.x}px`;
+  modal.style.top = `${pos.y}px`;
+
+  const images = Array.from(Object.keys(cluster.images));
+  const image = Full_Path_From_File_Name(images[Math.floor(Math.random() * images.length)]);
+  document.getElementById('cluster-modal-img').src = image;
+
+  const ul = document.getElementById('cluster-modal-list');
+  ul.innerHTML = '';
+
+  for (const image of images) {
+    ul.appendChild(CreateClusterRelatedThumbnail(image));
+  }
+}
+
+function CreateClusterRelatedThumbnail(filename) {
+  const li = document.createElement('li');
+  li.classList.add(['list-group-item', 'cluster-list-item', 'list-group-item-action']);
+  li.innerHTML = `
+          <img class="rounded img-thumbnail cluster-thumbnail-sm" src="${Full_Path_From_File_Name(filename)}">
+          <strong>${filename}</strong>          
+                `;
+
+  li.onclick = () => GENERAL_HELPER_FNS.Goto_Tagging_Entry(filename);
+  return li;
+}
+
+function CanvasToWindowXY(pos, canvas) {
+  const canvas_rect = canvas.getBoundingClientRect();
+  const canvas_w = canvas_rect.width;
+  const canvas_h = canvas_rect.height;
+
+  //translate position into canvas position
+  const cpos_x = Math.floor(canvas_w / 2) + pos.x;
+  const cpos_y = Math.floor(canvas_h / 2) + pos.y;
+
+  // translate into screen space positions
+  const x = canvas_rect.x + cpos_x;
+  const y = canvas_rect.y + cpos_y;
+
+  return { x, y };
 }
 
 Generate_Face_Map(settings);
